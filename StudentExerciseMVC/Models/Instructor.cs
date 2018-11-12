30c30080ae4ebbc0897b5ada38f9a9d4006a79b5
Kelly Cook <kellycook20878@gmail.com>
2018-11-12 09:39:13 -0600

    tryna push to github

--- StudentExerciseMVC/Models/Instructor.cs
+++ StudentExerciseMVC/Models/Instructor.cs
@@ -1,12 +1,25 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace StudentExercisesAPI.Data
 {
     public class Instructor
     {
         public int Id { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "How to find me on Slack")]
         public string SlackHandle { get; set; }
+
+        [Display(Name = "My Specialty is This!")]
         public string Specialty { get; set; }
+
+        public int CohortId { get; set; }
         public Cohort Cohort { get; set; }
     }
 }
